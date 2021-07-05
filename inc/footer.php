837b57b5c47336f5c290875ef4dffd59a74ca061
Junaid Bashir <junaid@crayondevelopers.com>
2021-07-05 21:27:32 +0100

    final fix sxript

--- inc/footer.php
+++ inc/footer.php
@@ -79,6 +79,7 @@
 <!-- Bootstrap core JavaScript -->
 <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
 <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css">
 
 <!-- Additional Scripts -->
 <script src="assets/js/custom.js"></script>
