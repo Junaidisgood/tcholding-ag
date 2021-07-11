33d972e32b530bbe2dc5044da92ab5006fdf4f34
Junaid Bashir <junaid@crayondevelopers.com>
2021-07-11 22:10:55 +0100

    final script fix

--- inc/footer.php
+++ inc/footer.php
@@ -77,9 +77,9 @@
 </div>
 
 <!-- Bootstrap core JavaScript -->
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js"></script>
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js">
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src=" https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js"></script>
+<script href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js"></script>
 
 <!-- Additional Scripts -->
 <script src="assets/js/custom.js"></script>
