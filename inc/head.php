99e34d956f60520ce30bdbe0861574d5e30df089
Junaid Bashir <junaid@crayondevelopers.com>
2021-07-26 21:52:50 +0100

    favicon & icon upload services listing fix

--- inc/head.php
+++ inc/head.php
@@ -2,10 +2,11 @@
 
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
+    <meta name="description" content="TC Holding AG (TCH) is a tier one bespoke Swiss joint-stock financial and investment firm that offers a variety of innovative services.">
     <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap" rel="stylesheet">
 
     <title>TC Holding AG</title>
+    <link rel="shortcut icon" type="image/jpg" href="assets/images/favicon.png" />
 
     <!-- Bootstrap core CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.css">
--- services.php
+++ services.php
@@ -34,32 +34,15 @@ require "inc/head.php";
           <ul>
             <li><a href='#global-wealth-management'>Global Wealth Management<i class="fa fa-angle-right"></i></a></li>
             <li><a href='#consultancy-advisory'>Consultancy/Advisory Services<i class="fa fa-angle-right"></i></a></li>
-            <li><a href='#merger-acquisition'>Mergers & Acquisitions<i class="fa fa-angle-right"></i></a></li>
             <li><a href='#banking'>Banking<i class="fa fa-angle-right"></i></a></li>
             <li><a href='#monetisation'>Monetisation<i class="fa fa-angle-right"></i></a></li>
-            <li><a href='#funding'>Funding<i class="fa fa-angle-right"></i></a></li>
-            <li><a href='#trade-finance'>Trade Finance<i class="fa fa-angle-right"></i></a></li>
             <li><a href='#structured-financing'>Structured Financing<i class="fa fa-angle-right"></i></a></li>
-          </ul><br>
-          <ul>
             <li><a href='#private-equity'>Private Equity<i class="fa fa-angle-right"></i></a></li>
             <li><a href='#infrastructure'>Infrastructure<i class="fa fa-angle-right"></i></a></li>
-            <li><a href='#venture-capital'>Venture Capital<i class="fa fa-angle-right"></i></a></li>
             <li><a href='#renewable'>Renewable<i class="fa fa-angle-right"></i></a></li>
-            <li><a href='#securitisation'>Securitisation<i class="fa fa-angle-right"></i></a></li>
-            <li><a href='#capital-restructuring'>Capital Restructuring<i class="fa fa-angle-right"></i></a></li>
             <li><a href='#real-estate'>Real Estate<i class="fa fa-angle-right"></i></a></li>
-            <li><a href='#bank-instrument-securities'>Bank Instruments & Securities<i class="fa fa-angle-right"></i></a></li>
-          </ul><br>
-          <ul>
             <li><a href='#investment'>Investment<i class="fa fa-angle-right"></i></a></li>
             <li><a href='#project-finance'>Project Finance<i class="fa fa-angle-right"></i></a></li>
-            <li><a href='#joint-venture'>Joint Venture<i class="fa fa-angle-right"></i></a></li>
-            <li><a href='#natural-resources'>Natural Resources<i class="fa fa-angle-right"></i></a></li>
-            <li><a href='#insurance'>Insurance<i class="fa fa-angle-right"></i></a></li>
-            <li><a href='#factoring'>Factoring<i class="fa fa-angle-right"></i></a></li>
-            <li><a href='#real-estate'>Real Estate<i class="fa fa-angle-right"></i></a></li>
-            <li><a href='#bank-instrument-securities'>Bank Instruments & Securities<i class="fa fa-angle-right"></i></a></li>
           </ul>
         </div>
         <div class="col-md-8">
@@ -78,13 +61,6 @@ require "inc/head.php";
                 <br><br>Pellentesque pretium, massa at placerat vehicula, neque turpis pulvinar tortor, eget convallis lorem odio non tortor. Donec massa est, fermentum sit amet felis ac, maximus luctus elit. Vivamus aliquet, dolor id imperdiet imperdiet, dui diam aliquet dui, a euismod metus enim ac velit. Vivamus eu tristique odio, vel tristique quam.
               </p>
             </article>
-            <article id='merger-acquisition'>
-              <img src="assets/images/single_service_03.jpg" alt="">
-              <h4>Mergers & Acquisitions</h4>
-              <p>Mauris lobortis quam id dictum dignissim. Donec pellentesque erat dolor, cursus dapibus turpis hendrerit quis. Suspendisse at suscipit arcu. Nulla sed erat lectus. Nulla facilisi. In sit amet neque sapien. Donec scelerisque mi at gravida efficitur. Nunc lacinia a est eu malesuada. Curabitur eleifend elit sapien, sed pulvinar orci luctus eget.
-                <br><br>Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nunc vel ultrices nulla, ac tincidunt eros. Aenean quis tellus velit. Praesent pretium justo non auctor condimentum.
-              </p>
-            </article>
             <article id='banking'>
               <img src="assets/images/single_service_04.jpg" alt="">
               <h4>Banking</h4>
@@ -99,20 +75,6 @@ require "inc/head.php";
                 <br><br>Proin eu molestie risus. Etiam suscipit pretium odio, at consectetur nisi. Sed ut dolor in augue cursus ultrices. Vivamus mauris turpis, auctor vel facilisis in, tincidunt vel diam. Sed vitae scelerisque orci. Nunc non magna orci. Aliquam commodo mauris ante.
               </p>
             </article>
-            <article id='funding'>
-              <img src="assets/images/single_service_02.jpg" alt="">
-              <h4>Funding</h4>
-              <p>Sed ut dolor in augue cursus ultrices. Vivamus mauris turpis, auctor vel facilisis in, tincidunt vel diam. Sed vitae scelerisque orci. Nunc non magna orci. Aliquam commodo mauris ante, quis posuere nibh vestibulum sit amet
-                <br><br>Pellentesque pretium, massa at placerat vehicula, neque turpis pulvinar tortor, eget convallis lorem odio non tortor. Donec massa est, fermentum sit amet felis ac, maximus luctus elit. Vivamus aliquet, dolor id imperdiet imperdiet, dui diam aliquet dui, a euismod metus enim ac velit. Vivamus eu tristique odio, vel tristique quam.
-              </p>
-            </article>
-            <article id='trade-finance'>
-              <img src="assets/images/single_service_03.jpg" alt="">
-              <h4>Trade Finance</h4>
-              <p>Mauris lobortis quam id dictum dignissim. Donec pellentesque erat dolor, cursus dapibus turpis hendrerit quis. Suspendisse at suscipit arcu. Nulla sed erat lectus. Nulla facilisi. In sit amet neque sapien. Donec scelerisque mi at gravida efficitur. Nunc lacinia a est eu malesuada. Curabitur eleifend elit sapien, sed pulvinar orci luctus eget.
-                <br><br>Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nunc vel ultrices nulla, ac tincidunt eros. Aenean quis tellus velit. Praesent pretium justo non auctor condimentum.
-              </p>
-            </article>
             <article id='structured-financing'>
               <img src="assets/images/single_service_04.jpg" alt="">
               <h4>Structured Financing</h4>
@@ -120,8 +82,6 @@ require "inc/head.php";
                 <br><br>Donec finibus maximus neque, vitae egestas quam imperdiet nec. Proin nec mauris eu tortor consectetur tristique. Etiam suscipit ante a odio consequat, in ornare lacus suscipit. Cras ac libero massa. Quisque rhoncus velit feugiat vulputate mollis. Donec nisl eros, malesuada sed nisi id, condimentum condimentum quam.
               </p>
             </article>
-          </section>
-          <section class="tabs-content">
             <article id='private-equity'>
               <img src="assets/images/single_service_01.jpg" alt="">
               <h4>Private Equity</h4>
@@ -136,32 +96,11 @@ require "inc/head.php";
                 <br><br>Pellentesque pretium, massa at placerat vehicula, neque turpis pulvinar tortor, eget convallis lorem odio non tortor. Donec massa est, fermentum sit amet felis ac, maximus luctus elit. Vivamus aliquet, dolor id imperdiet imperdiet, dui diam aliquet dui, a euismod metus enim ac velit. Vivamus eu tristique odio, vel tristique quam.
               </p>
             </article>
-            <article id='venture-capital'>
-              <img src="assets/images/single_service_03.jpg" alt="">
-              <h4>Venture Capital</h4>
-              <p>Mauris lobortis quam id dictum dignissim. Donec pellentesque erat dolor, cursus dapibus turpis hendrerit quis. Suspendisse at suscipit arcu. Nulla sed erat lectus. Nulla facilisi. In sit amet neque sapien. Donec scelerisque mi at gravida efficitur. Nunc lacinia a est eu malesuada. Curabitur eleifend elit sapien, sed pulvinar orci luctus eget.
-                <br><br>Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nunc vel ultrices nulla, ac tincidunt eros. Aenean quis tellus velit. Praesent pretium justo non auctor condimentum.
-              </p>
-            </article>
             <article id='renewable'>
               <img src="assets/images/single_service_04.jpg" alt="">
               <h4>Renewable</h4>
               <p>Integer vehicula sapien quis dolor efficitur, eget molestie eros tempus. Curabitur sollicitudin, tortor at suscipit volutpat, nisi arcu aliquet dui, vitae semper sem turpis quis libero. Quisque vulputate lacinia nisl ac lobortis. Ut ultricies maximus turpis, in sollicitudin ligula posuere vel.
                 <br><br>Donec finibus maximus neque, vitae egestas quam imperdiet nec. Proin nec mauris eu tortor consectetur tristique. Etiam suscipit ante a odio consequat, in ornare lacus suscipit. Cras ac libero massa. Quisque rhoncus velit feugiat vulputate mollis. Donec nisl eros, malesuada sed nisi id, condimentum condimentum quam.
-              </p>
-            </article>
-            <article id='securitisation'>
-              <img src="assets/images/single_service_01.jpg" alt="">
-              <h4>Securitisation</h4>
-              <p>Vivamus sed feugiat elit. Pellentesque pretium, massa at placerat vehicula, neque turpis pulvinar tortor, eget convallis lorem odio non tortor. Donec massa est, fermentum sit amet felis ac, maximus luctus elit. Vivamus aliquet, dolor id imperdiet imperdiet, dui diam aliquet dui, a euismod metus enim ac velit. Vivamus eu tristique odio, vel tristique quam.
-                <br><br>Proin eu molestie risus. Etiam suscipit pretium odio, at consectetur nisi. Sed ut dolor in augue cursus ultrices. Vivamus mauris turpis, auctor vel facilisis in, tincidunt vel diam. Sed vitae scelerisque orci. Nunc non magna orci. Aliquam commodo mauris ante.
-              </p>
-            </article>
-            <article id='capital-restructuring'>
-              <img src="assets/images/single_service_02.jpg" alt="">
-              <h4>Capital Restructuring</h4>
-              <p>Sed ut dolor in augue cursus ultrices. Vivamus mauris turpis, auctor vel facilisis in, tincidunt vel diam. Sed vitae scelerisque orci. Nunc non magna orci. Aliquam commodo mauris ante, quis posuere nibh vestibulum sit amet
-                <br><br>Pellentesque pretium, massa at placerat vehicula, neque turpis pulvinar tortor, eget convallis lorem odio non tortor. Donec massa est, fermentum sit amet felis ac, maximus luctus elit. Vivamus aliquet, dolor id imperdiet imperdiet, dui diam aliquet dui, a euismod metus enim ac velit. Vivamus eu tristique odio, vel tristique quam.
               </p>
             </article>
             <article id='real-estate'>
@@ -171,15 +110,6 @@ require "inc/head.php";
                 <br><br>Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nunc vel ultrices nulla, ac tincidunt eros. Aenean quis tellus velit. Praesent pretium justo non auctor condimentum.
               </p>
             </article>
-            <article id='bank-instrument-securities'>
-              <img src="assets/images/single_service_04.jpg" alt="">
-              <h4>Bank Instruments & Securities</h4>
-              <p>Integer vehicula sapien quis dolor efficitur, eget molestie eros tempus. Curabitur sollicitudin, tortor at suscipit volutpat, nisi arcu aliquet dui, vitae semper sem turpis quis libero. Quisque vulputate lacinia nisl ac lobortis. Ut ultricies maximus turpis, in sollicitudin ligula posuere vel.
-                <br><br>Donec finibus maximus neque, vitae egestas quam imperdiet nec. Proin nec mauris eu tortor consectetur tristique. Etiam suscipit ante a odio consequat, in ornare lacus suscipit. Cras ac libero massa. Quisque rhoncus velit feugiat vulputate mollis. Donec nisl eros, malesuada sed nisi id, condimentum condimentum quam.
-              </p>
-            </article>
-          </section>
-          <section class="tabs-content">
             <article id='investment'>
               <img src="assets/images/single_service_01.jpg" alt="">
               <h4>Investment</h4>
@@ -192,55 +122,6 @@ require "inc/head.php";
               <h4>Project Finance</h4>
               <p>Sed ut dolor in augue cursus ultrices. Vivamus mauris turpis, auctor vel facilisis in, tincidunt vel diam. Sed vitae scelerisque orci. Nunc non magna orci. Aliquam commodo mauris ante, quis posuere nibh vestibulum sit amet
                 <br><br>Pellentesque pretium, massa at placerat vehicula, neque turpis pulvinar tortor, eget convallis lorem odio non tortor. Donec massa est, fermentum sit amet felis ac, maximus luctus elit. Vivamus aliquet, dolor id imperdiet imperdiet, dui diam aliquet dui, a euismod metus enim ac velit. Vivamus eu tristique odio, vel tristique quam.
-              </p>
-            </article>
-            <article id='joint-venture'>
-              <img src="assets/images/single_service_03.jpg" alt="">
-              <h4>Joint Venture</h4>
-              <p>Mauris lobortis quam id dictum dignissim. Donec pellentesque erat dolor, cursus dapibus turpis hendrerit quis. Suspendisse at suscipit arcu. Nulla sed erat lectus. Nulla facilisi. In sit amet neque sapien. Donec scelerisque mi at gravida efficitur. Nunc lacinia a est eu malesuada. Curabitur eleifend elit sapien, sed pulvinar orci luctus eget.
-                <br><br>Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nunc vel ultrices nulla, ac tincidunt eros. Aenean quis tellus velit. Praesent pretium justo non auctor condimentum.
-              </p>
-            </article>
-            <article id='natural-resources'>
-              <img src="assets/images/single_service_04.jpg" alt="">
-              <h4>Natural Resources</h4>
-              <p>Integer vehicula sapien quis dolor efficitur, eget molestie eros tempus. Curabitur sollicitudin, tortor at suscipit volutpat, nisi arcu aliquet dui, vitae semper sem turpis quis libero. Quisque vulputate lacinia nisl ac lobortis. Ut ultricies maximus turpis, in sollicitudin ligula posuere vel.
-                <br><br>Donec finibus maximus neque, vitae egestas quam imperdiet nec. Proin nec mauris eu tortor consectetur tristique. Etiam suscipit ante a odio consequat, in ornare lacus suscipit. Cras ac libero massa. Quisque rhoncus velit feugiat vulputate mollis. Donec nisl eros, malesuada sed nisi id, condimentum condimentum quam.
-              </p>
-            </article>
-            <article id='insurance'>
-              <img src="assets/images/single_service_01.jpg" alt="">
-              <h4>Insurance</h4>
-              <p>Vivamus sed feugiat elit. Pellentesque pretium, massa at placerat vehicula, neque turpis pulvinar tortor, eget convallis lorem odio non tortor. Donec massa est, fermentum sit amet felis ac, maximus luctus elit. Vivamus aliquet, dolor id imperdiet imperdiet, dui diam aliquet dui, a euismod metus enim ac velit. Vivamus eu tristique odio, vel tristique quam.
-                <br><br>Proin eu molestie risus. Etiam suscipit pretium odio, at consectetur nisi. Sed ut dolor in augue cursus ultrices. Vivamus mauris turpis, auctor vel facilisis in, tincidunt vel diam. Sed vitae scelerisque orci. Nunc non magna orci. Aliquam commodo mauris ante.
-              </p>
-            </article>
-            <article id='funding'>
-              <img src="assets/images/single_service_02.jpg" alt="">
-              <h4>Funding</h4>
-              <p>Sed ut dolor in augue cursus ultrices. Vivamus mauris turpis, auctor vel facilisis in, tincidunt vel diam. Sed vitae scelerisque orci. Nunc non magna orci. Aliquam commodo mauris ante, quis posuere nibh vestibulum sit amet
-                <br><br>Pellentesque pretium, massa at placerat vehicula, neque turpis pulvinar tortor, eget convallis lorem odio non tortor. Donec massa est, fermentum sit amet felis ac, maximus luctus elit. Vivamus aliquet, dolor id imperdiet imperdiet, dui diam aliquet dui, a euismod metus enim ac velit. Vivamus eu tristique odio, vel tristique quam.
-              </p>
-            </article>
-            <article id='factoring'>
-              <img src="assets/images/single_service_03.jpg" alt="">
-              <h4>Factoring</h4>
-              <p>Mauris lobortis quam id dictum dignissim. Donec pellentesque erat dolor, cursus dapibus turpis hendrerit quis. Suspendisse at suscipit arcu. Nulla sed erat lectus. Nulla facilisi. In sit amet neque sapien. Donec scelerisque mi at gravida efficitur. Nunc lacinia a est eu malesuada. Curabitur eleifend elit sapien, sed pulvinar orci luctus eget.
-                <br><br>Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nunc vel ultrices nulla, ac tincidunt eros. Aenean quis tellus velit. Praesent pretium justo non auctor condimentum.
-              </p>
-            </article>
-            <article id='real-estate'>
-              <img src="assets/images/single_service_04.jpg" alt="">
-              <h4>Real Estate</h4>
-              <p>Integer vehicula sapien quis dolor efficitur, eget molestie eros tempus. Curabitur sollicitudin, tortor at suscipit volutpat, nisi arcu aliquet dui, vitae semper sem turpis quis libero. Quisque vulputate lacinia nisl ac lobortis. Ut ultricies maximus turpis, in sollicitudin ligula posuere vel.
-                <br><br>Donec finibus maximus neque, vitae egestas quam imperdiet nec. Proin nec mauris eu tortor consectetur tristique. Etiam suscipit ante a odio consequat, in ornare lacus suscipit. Cras ac libero massa. Quisque rhoncus velit feugiat vulputate mollis. Donec nisl eros, malesuada sed nisi id, condimentum condimentum quam.
-              </p>
-            </article>
-            <article id='bank-instrument-securities'>
-              <img src="assets/images/single_service_03.jpg" alt="">
-              <h4>Bank Instruments & Securities</h4>
-              <p>Mauris lobortis quam id dictum dignissim. Donec pellentesque erat dolor, cursus dapibus turpis hendrerit quis. Suspendisse at suscipit arcu. Nulla sed erat lectus. Nulla facilisi. In sit amet neque sapien. Donec scelerisque mi at gravida efficitur. Nunc lacinia a est eu malesuada. Curabitur eleifend elit sapien, sed pulvinar orci luctus eget.
-                <br><br>Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nunc vel ultrices nulla, ac tincidunt eros. Aenean quis tellus velit. Praesent pretium justo non auctor condimentum.
               </p>
             </article>
           </section>
